98802eda99dd1ee2a673028d251f0085966d602d
Janek <lxndr75@gmail.com>
2026-09-17 10:35:44 +0200

    feat(main): integrate Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import '@fontsource-variable/manrope/wght.css'
 
 import './index.css'
@@ -10,6 +11,8 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
+
 
